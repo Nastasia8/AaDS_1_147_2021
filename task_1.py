a79a18bc6a2d4e6e75e6447855fb4ac9eb9aee00
Kooperatorko <71215527+Kooperatorko@users.noreply.github.com>
2021-02-06 11:23:47 +0300

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,9 @@
+o = 0
+p = 0
+i = 0
+for i in 'Python is an interpreted, high-level and general-purpose programming language. Pythons design philosophy emphasizes code readability with its notable use of significant whitespace. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects. Python is dynamically-typed and garbage-collected.'.lower():
+    if i == 'p':
+        p += 1
+    if i == 'o':
+        o += 1
+print('Букв p и o в этом тексте:\np = ', p, '; o = ', o)
