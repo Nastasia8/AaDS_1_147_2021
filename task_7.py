4f713ec85d1378f452ed6edcb9e7c4fae9291a7a
Kooperatorko <71215527+Kooperatorko@users.noreply.github.com>
2021-02-08 22:43:49 +0300

    Update task_7.py

--- task_7.py
+++ task_7.py
@@ -1,4 +1,4 @@
-from math import *
+from math import sqrt
 
 print('Введите x: ', end='')
 x = float(input())
@@ -7,13 +7,13 @@ y = float(input())
 print('Введите z: ', end='')
 z = float(input())
 
-def func(D):
+def func(x, y, z):
+    D = y*y - 4*x*z
     if D < 0:
         print('Корней нет')
     elif D == 0:
         print('У уравнения единсвенный корень = ',round((-y)/(2*x)), 3)
     else:
-        print('У уравнения два корня\nПервый корень = ', round(((-y)+sqrt(D))/(2*x), 3), '\nВторой корень = ', round(((-y)-sqrt(D))/(2*x)), 3)
+        print('У уравнения два корня:\nПервый корень = ', round(((-y)+sqrt(D))/(2*x), 3), '\nВторой корень = ', round(((-y)-sqrt(D))/(2*x)), 3)
 
-func(y*y - 4*x*z)
-    
+func(x, y, z)
