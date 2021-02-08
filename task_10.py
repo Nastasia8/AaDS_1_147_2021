89f31aa7aad0095a6f8a754aca0b2549e787e77a
Kooperatorko <71215527+Kooperatorko@users.noreply.github.com>
2021-02-08 21:29:17 +0300

    Update task_10.py

--- task_10.py
+++ task_10.py
@@ -1,13 +1,13 @@
-i = 0
-print('Введите степень x: ', end='')
-y = float(input())
 print('Введите степень y: ', end='')
+y = float(input())
+print('Введите степень z: ', end='')
 z = float(input())
-ps = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+ps = list(range(1, 21))
+i = 0
 
-while i < 19:
+while i < len(ps):
     if ps[i] % 2 != 0:
-        ps[i] = ps[i]**(y**z)
+        ps[i] = ps[i]**y**z
     i += 1
 
 print('[', end='')
