d4d01ff29a3048217f7bbf34494cf268f9691811
Kooperatorko <71215527+Kooperatorko@users.noreply.github.com>
2021-02-09 16:13:36 +0300

    Update task_2.2.py

--- task_2.2.py
+++ task_2.2.py
@@ -1,9 +1,7 @@
 animals = ['tiger', 'leopard', 'elephant', 'Fox', 'wolf', 'camel', 'raccoon']
 
 def func(animals):
-    j = animals[0]
-    animals[0] = animals[6]
-    animals[6] = j
+    animals[0], animals[6] = animals[6], animals[0]
     return(animals)
 
 print(func(animals))
