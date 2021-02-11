2664fdecb4cf031facb9505796a4b3db553f6bf8
Kooperatorko <71215527+Kooperatorko@users.noreply.github.com>
2021-02-11 12:52:51 +0300

    Update task_7.py

--- 09.02.2021/task_7.py
+++ 09.02.2021/task_7.py
@@ -1,4 +1,13 @@
 import numpy as np
 
-m_sqr_arr = np.array([[3, 7, 2], [1, 4, 3], [5, 3, 2]])
-print(m_sqr_arr)
+def func(A, i):
+    A = np.array(A)
+    n = len(A[0]) - 1
+    print('До преобразования:\n', A)
+    while i < len(A):
+        A[i][n] = A[i][n] * 2
+        i += 1
+        n -= 1
+    print('После преобразования:\n', A)
+    
+func([[3, 7, 2], [1, 4, 3], [5, 3, 2]], 0)
