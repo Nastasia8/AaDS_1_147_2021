4ffa4b5787efef1fb11c9a360cc7141c9dd7bf7f
Alexey <37597550+Borshecs@users.noreply.github.com>
2021-02-15 08:08:59 +0300

    Create task4.py

---
+++ task4.py
@@ -0,0 +1,16 @@
+text = 'Hello hi how hello are and you I am fine thank you and you hello You Thank And'.lower().split()
+x = []
+i = 0
+
+while i < len(text):
+    j = -1
+    k = 0
+    while j < len(text):
+        if text[i] == text[j]:
+            k += 1
+        j += 1
+    i += 1
+    x.insert(i, k)
+    
+slova = dict(zip(text, x))
+print(slova)
