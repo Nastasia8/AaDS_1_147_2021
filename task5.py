cba230483ba523583a87484080fec830ad6d3b15
Alexey <37597550+Borshecs@users.noreply.github.com>
2021-02-15 08:11:00 +0300

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,17 +1,17 @@
-def main():
-    funk()
 
+text = 'Hello hi how hello are and you I am fine thank you and you hello You Thank And'.lower().split()
+x = []
+i = 0
 
-
-
-
-
-def funk(): #не работает
-    a = {'Hello hi how hello are and you I am fine thank you and you hello You Thank And'}
-
-
-
-
-
-
-main()
+while i < len(text):
+    j = -1
+    k = 0
+    while j < len(text):
+        if text[i] == text[j]:
+            k += 1
+        j += 1
+    i += 1
+    x.insert(i, k)
+    
+slova = dict(zip(text, x))
+print(slova)
