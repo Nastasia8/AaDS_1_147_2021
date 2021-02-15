f56e5c3f3a030ea3ba88f953225dd63c2bb998ab
Алексей Овсянников <71230467+13212076298@users.noreply.github.com>
2021-02-15 08:32:54 +0300

    Add files via upload

---
+++ main.py6.py
@@ -0,0 +1,19 @@
+
+def main():
+    funk()
+
+
+
+def funk():
+    diction = [3, 8, 2, 1, 4, 7, 5, 6, 10, 9]
+    new_dict = []
+
+    for i in diction:
+        if i%2 == 0:
+            new_dict.append(i)
+    print(new_dict)
+
+
+
+
+main()
