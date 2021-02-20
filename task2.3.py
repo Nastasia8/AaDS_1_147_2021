29f326b4052146acd37a7afc13b6a51569446579
Alexey <37597550+Borshecs@users.noreply.github.com>
2021-02-20 10:50:23 +0300

    Update task2.3.py

--- task2.3.py
+++ task2.3.py
@@ -1,16 +1,10 @@
-from  random import randint
-
-def main():
-    slov()
-
-def slov():
-    a = ("Crime and Punishment", 1, 331, 1866, "The Master and Margarita", 1, 470, 1966, "War and Peace", 4, 1274, 1869, "And Quiet Flows the Don", 4, 1600, 1940)
-    dct = {}
-   
-    for v in a:
-        dct_one = dct.fromkeys(a, randint(0,1000))
-    print(dct_one)
-
-  
-
-main()
+words = ["Crime and Punishment", 1, 331, 1866, "The Master and Margarita", 1, 470, 1966, "War and Peace", 4, 1274, 1869, "And Quiet Flows the Don", 4, 1600, 1940]
+d={}
+s = ""
+for word in words:
+    if type(word) == str:
+        d[word] = []
+        s =word
+    else:
+        d[s].append(word)
+print(d)
