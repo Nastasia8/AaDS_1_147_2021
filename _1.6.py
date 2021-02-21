bfbbbace521f5c1b539aa26c8fd3f1a95b401992
liza2307 <71287515+liza2307@users.noreply.github.com>
2021-02-21 13:36:36 +0300

    Add files via upload

---
+++ _1.6.py
@@ -0,0 +1,15 @@
+x = int(input("Введите x: "))
+y = int(input("Введите y: "))
+z = int(input("Введите z: "))
+
+D=y*y - 4*x*z
+
+if D<0:
+	print("корней нет")
+if D==0:
+	a=-y/2*x
+	print(a)
+if D>0:
+	a1=(-y + D**0.5) / (2 * x)
+	a2=(-y - D**0.5)/ (2 * x)
+	print(a1,a2)
