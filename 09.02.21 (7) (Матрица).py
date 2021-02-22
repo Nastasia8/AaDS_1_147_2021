91d966df2bbfe8be98b4ea975e7379fb1a86f60d
vladmet1 <71036195+vladmet1@users.noreply.github.com>
2021-02-22 20:50:50 +0300

    Update 09.02.21 (7) (Матрица).py

--- 09.02.21 (7) (Матрица).py
+++ 09.02.21 (7) (Матрица).py
@@ -2,11 +2,11 @@ a = [[1,2,3],
      [4,5,6],
      [7,8,9]]
 
-def m(a):
-    print(a)
-    for i in range(len(a)):
-        a[i][len(a)-i-1] *= 2
-    print(a)
+def print_massive(arr):
+    for i in range(len(arr)):
+      for j in range(len(arr[i])):
+        print(arr[i][j] , end= "\t")
+      print()
 
-print(m(a))
+print_massive(a)
 
