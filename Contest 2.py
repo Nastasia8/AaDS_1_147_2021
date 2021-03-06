6ae27bf5db82088dc490f169ddb0a90fa819160d
DustofTimeYT <71147997+DustofTimeYT@users.noreply.github.com>
2021-03-06 11:47:35 +0300

    Add files via upload

--- Contest 2.py
+++ Contest 2.py
@@ -3,13 +3,10 @@ n = int(input())
 for i in range(n):
     key,valve =map(int,input().split())
     num.append([key,valve])
-for i in range(len(num)-1):
-    for j in range(len(num)-i-1):
-        if num[j][1] > num[j+1][1]:
+for i in range(n-1):
+    for j in range(n-i-1):
+        if num[j][1] < num[j+1][1]:
             num[j], num[j+1] = num[j+1], num[j]
-        elif num[j][1] == num[j+1][1]:
-            if num[j][0] > num[j+1][0]:
-                num[j], num[j+1] = num[j+1], num[j]
-            
-# for i in range(len(num)-1):
-print(*num, sep=" ")
+        if num[j][1] == num[j+1][1] and num[j][0] > num[j+1][0]:
+            num[j], num[j+1] = num[j+1], num[j]
+[print(i[0],i[1])for i in num]
