4ef95a0f19283a0d21e80658f7c97f41e9e1f565
Alexey <37597550+Borshecs@users.noreply.github.com>
2021-03-09 15:40:36 +0300

    Add files via upload

--- модуль 2 ( сорт)/sort.py
+++ модуль 2 ( сорт)/sort.py
@@ -1,8 +1,7 @@
 import re
 
 i = int(input())
-raw = input()
-entermass = [int(i) for i in raw.split(' ') if i.isdigit()]
+entermass = list(map(int,input().split()))[:i]
 
 
 
@@ -10,13 +9,12 @@ def buble_sort(mylist):
     last = len(mylist) - 1
     for x in range(0,last):
         for y in range(0,last-x):
-            for i in mylist:
-                print(i, end = ' ')
-            print()
             if mylist[y] > mylist[y+1]:
                 mylist[y], mylist [y+1] = mylist [y+1], mylist[y]
+                for i in mylist:
+                    print(i, end = ' ')
+                print()
+            
     return mylist
 
 newlist = buble_sort(entermass).copy()
-for i in newlist:
-    print(i, end = ' ')
