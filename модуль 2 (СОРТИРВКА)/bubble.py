8866d5f41191cde3f6a7811e1c490d8baeec50a7
Алексей Овсянников <71230467+13212076298@users.noreply.github.com>
2021-03-09 13:30:47 +0300

    Add files via upload

---
+++ модуль 2 (СОРТИРВКА)/bubble.py
@@ -0,0 +1,19 @@
+print('Введите нужное количесиыо элементов и сами эелементы. После введенего элемента нажмите  Enter')
+entermass = []
+n = int(input())
+for i in range(n):
+    entermass.append(int(input()))
+print(entermass)
+
+def buble_sort(mylist):
+    last = len(mylist) - 1
+    for x in range(0,last):
+        for y in range(0,last-x):
+            print(mylist)
+            if mylist[y] > mylist[y+1]:
+                mylist[y], mylist [y+1] = mylist [y+1], mylist[y]
+    return mylist
+
+print('Old list: ', entermass)
+newlist = buble_sort(entermass).copy()
+print('New list; ', newlist)
