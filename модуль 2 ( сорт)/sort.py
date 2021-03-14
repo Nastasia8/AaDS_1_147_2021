44f71a20c77ca38c8c186c64e127373f9f02357f
Alexey <37597550+Borshecs@users.noreply.github.com>
2021-03-14 20:07:00 +0300

    Update sort.py

--- модуль 2 ( сорт)/sort.py
+++ модуль 2 ( сорт)/sort.py
@@ -1,20 +1,27 @@
-import re
-
 i = int(input())
-entermass = list(map(int,input().split()))[:i]
+entermass = list(map(int, input().split()))[:i]
 
 
 
 def buble_sort(mylist):
+    sorts = 0
     last = len(mylist) - 1
-    for x in range(0,last):
-        for y in range(0,last-x):
-            if mylist[y] > mylist[y+1]:
-                mylist[y], mylist [y+1] = mylist [y+1], mylist[y]
+    for x in range(0, last):
+        for y in range(0, last - x):
+            if mylist[y] > mylist[y + 1]:
+                mylist[y], mylist[y + 1] = mylist[y + 1], mylist[y]
                 for i in mylist:
-                    print(i, end = ' ')
+                    sorts +=1
+                    print(i, end=' ')
+
                 print()
-            
-    return mylist
+    if sorts == 0:
+        print("0")
+
+
+    return (mylist)
+
 
 newlist = buble_sort(entermass).copy()
+
+
