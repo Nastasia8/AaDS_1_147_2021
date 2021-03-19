2404844382148048394ae699c357a7e3248c163b
Alexey <37597550+Borshecs@users.noreply.github.com>
2021-03-19 18:19:49 +0300

    Update 10.py

--- lab1/10.py
+++ lab1/10.py
@@ -1,6 +1,12 @@
-def calc():
+def main():
     y = int(input())
     z = int(input())
+    calc(y,z)
+
+def calc(y,z):
+    
     for i in range(1,21,2):
-        print(i**(y**z))
-calc()
+        print(i**y**z, end="")
+
+
+main() 
