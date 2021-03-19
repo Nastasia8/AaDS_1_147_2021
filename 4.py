0953ee7a6a52ba1fdb71c7cf61c1380ffacf9b77
Alexey <37597550+Borshecs@users.noreply.github.com>
2021-03-19 18:15:56 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,15 +1,20 @@
-  
-def funk():
-    m=0
-    n = 1
-    if (m >= 100000 or m < 1000000):
+def main(): 
+     m = int(input("Введите число: "))
+     
+
+     print(funk(m))
+
+
+def funk(zed):
+     n=1
+     if (zed >= 100000 and zed < 1000000):
         for i in range(0, 6):
-            n *= m % 10
-            m = m // 10
-    else:
-        print("Ошибка")
-    return n
+            n *= zed % 10
+            zed = zed // 10
+        else:
+            print("Ошибка")
+        
+     return n
 
 
-m = int(input("Введите число: "))
-print(funk())
+main()
