031b9c8c63230a0d2c004d70c7018efe2e0fbe26
Alexey <37597550+Borshecs@users.noreply.github.com>
2021-03-19 18:22:39 +0300

    Update 2.py

--- lab1/2.py
+++ lab1/2.py
@@ -1,14 +1,19 @@
 from math import pi, sqrt
-
-def frequency():
+def main():
     T = float(input("Введите T\n"))
-    w = ((2 * pi)/T)
-    print(w)
-def period():
+    frequency(T)
     m = float(input("Введите m\n"))
     k = float(input("Введите k\n"))
+    period(m,k)
+
+
+def frequency(T):
+    
+    w = ((2 * pi)/T)
+    print(w)
+def period(m,k):
+  
     T = 2*pi*(sqrt(m/k))
     print(T)
     
-frequency()
-period()
+main()     
