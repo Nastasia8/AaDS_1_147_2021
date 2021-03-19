5a86056340f0057a4a01253b0dd0fb9d9d83991d
Alexey <37597550+Borshecs@users.noreply.github.com>
2021-03-19 18:34:09 +0300

    Update 7.py

--- lab1/7.py
+++ lab1/7.py
@@ -1,16 +1,22 @@
 from math import sqrt
-def calc():
+def main():
     x = int(input("Введите x\n"))
     y = int(input("Введите y\n"))
     z = int(input("Введите z\n"))
-    D = y**2-4*x*z
-    if D < 0:
+    calc(x, y, z)
+
+
+def calc(x, y, z):
+     D = y**2-4*x*z
+     if D < 0:
         print("Корней нет")
-    if D == 0:
+     if D == 0:
         f1 = -y/(2*x)
         print(f1)
-    if D >= 0:
+     if D >= 0:
         f1 = (-y + sqrt(D))/(2*x)
         f2 = (-y - sqrt(D))/(2*x)
-        print(f1,f2)
-calc()
+        print(round(f1, 3),round(f2, 3))
+
+
+main()
