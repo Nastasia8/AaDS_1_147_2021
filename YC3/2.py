63ac4da89816263b5a04debfdf5ff13e3aaa820b
Игорь Амелин <amelin.it@gmail.com>
2021-03-20 11:12:31 +0300

    20.03.21

--- YC3/2.py
+++ YC3/2.py
@@ -1,18 +1,13 @@
-n = int(input())
-a = list(map(int,input().split()))[:n]
-	
-sorts = 1
-passes = 0
-while sorts != 0:
-	sorts = 0
-	for i in range(len(a) - 1):
-		if a[i] > a[i + 1]:
-			a[i], a[i + 1] = a[i + 1],a[i]
-			for b in range(len(a)):
-				print(a[b], end = ' ')
-			print()
-			sorts+=1
-			passes +=1
-	if (sorts == 0 and passes == 0):
-			print(0)
-		
+def rotate(l, n):
+    return l[-n:] + l[:-n]
+
+counter = 0
+S = list(input())
+T = list(input())
+for i in range(len(S)):
+    S = rotate(S,0)
+    if S == T:
+        counter=i+1
+if counter == 0:
+    counter = -1
+print(counter)
