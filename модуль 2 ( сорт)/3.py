e22c1b737050bba6c6e123b14ff77295abfd5f5a
Alexey <37597550+Borshecs@users.noreply.github.com>
2021-03-20 09:58:58 +0300

    Update 3.py

--- модуль 2 ( сорт)/3.py
+++ модуль 2 ( сорт)/3.py
@@ -1,9 +1,39 @@
-n = int(input())
-x = 0
-array = list(map(int,input().split()))
+import operator
+def main():
+     n = int(input())
+     m =[]
+     m = list(map(int,input().split()))
+     sort(m)
 
-for i in range (0,n-1):
-    for j in range (i+1,n):
-        if array[i]>array[j] and i<j:
-            x+=1
-print (x)
+
+def sort(arr, compare=operator.lt):
+    if len(arr) < 2:
+        return arr
+    else:
+        mid = len(arr)//2
+        left = sort(arr[:mid], compare)
+        right = sort(arr[mid:], compare)
+        print (mid,mid+1, right[0], left[-1])
+        return merge(left,right, compare)
+def merge(left, right, compare):
+    massiv = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if compare(left[i], right[j]):
+            massiv.append(left[i])
+            i+=1
+        else:
+            massiv.append(right[j])
+            j+=1
+    while i < len(left):
+        massiv.append(left[i])
+        i+=1
+    while j < len(right):
+        massiv.append(right[j])
+        j+=1
+    for i in massiv:
+                    
+                    print(i, end=' ')
+    
+
+main()    
