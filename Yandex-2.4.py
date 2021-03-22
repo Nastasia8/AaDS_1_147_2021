830002e47f8171f80b4b956b9068be90fcb9e134
Nikrybkin <71150246+Nikrybkin@users.noreply.github.com>
2021-03-22 21:44:16 +0300

    Add files via upload

--- Yandex-2.4.py
+++ Yandex-2.4.py
@@ -1,15 +1,35 @@
-a = int(input())
+def merge(one, two):
+    spis = []
+    i = 0
+    j = 0
+    inversion = 0
+    while i < len(one) and j < len(two):
+        if one[i] <= two[j]:
+            spis.append(one[i])
+            i += 1
+        else:
+            spis.append(two[j])
+            j += 1
+            inversion += len(one) - i
+    while i < len(one):
+        spis.append(one[i])
+        i += 1
+    while j < len(two):
+        spis.append(two[j])
+        j += 1
+    return spis, inversion
 
-def func(inversion, a):    
-    count = 0
-    
-    for i in range(a):
-        for j in range(i + 1, a):
-            if array[i] > array[j]:
-                count += 1
-    return count
+def merge_sort(m):
+    n = len(m)
+    if n <= 1:
+        return m, 0
+    middle = n//2
+    one, left = merge_sort(m[0:middle])    
+    two, right = merge_sort(m[middle:n])
+    spis, inversion = merge(one, two)
+    return spis, inversion + left + right    
 
-
-
-array = list(map(int, input().split()))
-print(func(array, a))
+n = int(input())
+m = list(map(int, input().split(" ")))
+m, fin = merge_sort(m)
+print(fin)
