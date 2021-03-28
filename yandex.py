2066e57b6dd9a473b84d0eb2e47ff47613dc2d23
swbonjour <71038541+swbonjour@users.noreply.github.com>
2021-03-28 14:45:05 +0300

    yandex.py

--- yandex.py
+++ yandex.py
@@ -1,3 +1,4 @@
+#sort 1
 N = int(input())
 numbers = [int(i) for i in input().split()][:N]
 
@@ -12,3 +13,24 @@ for i in range(N-1):
     
 if check == 0:
 	print(0)
+	
+#sort 2
+N = int(input())
+a = []
+
+for i in range(N):
+    a.append([int(j) for j in input().split()][:N])
+
+for i in range(len(a)-1):
+    for j in range(len(a)-i-1):
+        if a[j+1][1] > a[j][1]:
+            a[j+1],a[j] = a[j],a[j+1]
+
+for i in range(len(a)-1):
+    for j in range(len(a)-i-1):
+        if a[j+1][1] == a[j][1]:
+            if a[j+1][0] < a[j][0]:
+                a[j+1],a[j] = a[j],a[j+1]
+
+for i in range(len(a)):
+    print(*a[i])
