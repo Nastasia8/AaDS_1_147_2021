f8ca22113dcfd0bb9d367f89928d4b5e82db7b76
DustofTimeYT <71147997+DustofTimeYT@users.noreply.github.com>
2021-03-30 23:19:06 +0300

    1

--- Contest2.4.py
+++ Contest2.4.py
@@ -11,32 +11,8 @@ def preph(s):
     return p
 
 
-# s = input()  # 'aaabdbaa' 'affaffaffaffaffaff''aaabdbaaaaabdbaa''abcabcabc'
-# print(len(s))
-# s_t = s+"$"+s*2
-# p = preph(s_t)
-# print(p, s_t)
-# ind_start = p.index(max(p))
-# p[ind_start] = -1
-# ind_end = p.index(max(p))
-# print(ind_end-ind_start)
-s = T = input()
-if s[0]*len(s) == s:
-    print(1)
-else:
-    i = 0
-    while s and len(s) > 1 and s[0] == s[-1]:
-        i += 1
-        s = s[1:-1]
-
-    if s != T[0]:
-        s = len(s)+i
-    else:
-        s = i
-    t = T[:s]
-    p = preph(t+'&'+T)
-    p = p[len(t)+1:]
-    for i in p:
-        if i == 0:
-            s += 1
-    print(s)
+s = input()
+s_t = s+"$"+s
+a = preph(s_t)
+p0 = a[-1]-a[len(s)-1]
+print(p0)
