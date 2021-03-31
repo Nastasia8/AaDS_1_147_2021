7ca7ddd709d8368588fc14c1237986118c0f5dd8
Alexey <37597550+Borshecs@users.noreply.github.com>
2021-03-31 20:37:14 +0300

    Update 3.py

--- Яндекс Контест 3/3.py
+++ Яндекс Контест 3/3.py
@@ -1,17 +1,4 @@
-# def preph (s):
-#     p=[0]*len(s)
-#     for i in range (len(s)-1):
-#         k=p[i]
-#         while k>0 and s[i+1]!=s[k]:
-#             k=p[k-1]
-#         if s[i+1]==s[k]:
-#             p[i+1]=k+1
-#         else: 
-#             p[i+1]=0
-#     return p
 
-# s=input()
-# print (preph(s))
 
 def prefix(s):
     v = [0]*len(s)
