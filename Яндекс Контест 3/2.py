3c0b0ea850c1822564a443a7f7fb8baa4c014f68
Alexey <37597550+Borshecs@users.noreply.github.com>
2021-03-31 20:21:11 +0300

    Add files via upload

--- Яндекс Контест 3/2.py
+++ Яндекс Контест 3/2.py
@@ -1,19 +1,34 @@
-s=input()
-t=input()
-n=len(s)
-k=-1
-for i in range (n+1):
-    s1=list(s)
-    s=''
+def get_hash(s,x,p):
+    hs=0
+    for i in range (len(s)):
+        hs=(hs*x+ord(s[i]))%p
+    return hs
+def fun (s,t,x,p):
+    if s!=t:
+        k=1 
+        t=t[1:]+t[0]#bcdza
+        hs=get_hash(s,x,p)
+        ht=get_hash(t,x,p)
+        xt=1 
+        for i in range (len(s)-1):
+            xt=(xt*x)%p
+        for i in range (len(t)-1):
+            if hs==ht:
+                break
+            else:
+                ht=(x*(ht-ord(t[i])*xt)+ord(t[i]))%p
+        if hs==ht:
+            print(k)
+        else:
+            print(-1)
+    else:
+        print(0)
+    
+def main():
+    s=input()
+    t=input()
+    x=26
+    p=1e9+7
+    fun(s,t,x,p)
 
-    b=s1[-1]
-    for j in range (n-1,0,-1):
-        s1[j]=s1[j-1]
-    s1[0]=b
-
-    for j in range(len(s1)):
-        s+=s1[j]
-    if s==t:
-        k=i+1
-        break
-print(k)
+main()    
