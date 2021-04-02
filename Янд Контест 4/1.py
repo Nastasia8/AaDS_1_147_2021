e74d950e063988c3c5395f125e22edaad54c60a1
Alexey <37597550+Borshecs@users.noreply.github.com>
2021-04-02 22:52:51 +0300

    Update 1.py

--- Янд Контест 4/1.py
+++ Янд Контест 4/1.py
@@ -1,60 +1,17 @@
-# def psp(line):
-#     open = "<({"
-#     close = ">)}"
-#     stack = []
-#     p = 0
-#     for i in line:
-#         if (i in open):
-#             if (stack == []):
-#                 stack.append(open.index(i))
-#             else:
-#                 p = p  + 1   
-#         elif i in close:
-#             if stack and stack[-1] == close.index(i):
-#                 stack.pop()
-#             else:
-#                 p = p + 1
-
-#     return p
-
-# def main():
-#     text = "))((("
-#     print(psp(text))
-
-# main()    
-
-
-
 def psp(line):
-    open = "<({"
-    close = ">)}"
     stack = []
     p = 0
-    arr = list(line)
-    for i, sims in enumerate(arr):
-        if (sims in open):
-            if ( arr[i] == arr[i-1]) and (arr[i]):
-                p = p + 1
-            else:
-                 stack.append(open.index(sims))    
-
-               
-             
-        elif sims in close:
-            if stack and stack[-1] == close.index(sims):
+    for i in range(len(line)):
+        if line[i] == "(":
+            stack.append(line[i])
+        else:
+            if stack:
                 stack.pop()
-            else:
                 p = p + 1
-
     return p
 
 def main():
     text = input()
-    print(psp(text))
+    print(len(text) - 2*psp(text))
 
-main()    
-
-                
-
-
-        
+main()
