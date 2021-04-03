68e3d2295c8038c2dc421755f2146f7d04d91c82
Alexey <37597550+Borshecs@users.noreply.github.com>
2021-04-03 09:54:38 +0300

    Update 3.py

--- Яндекс Контест 3/3.py
+++ Яндекс Контест 3/3.py
@@ -1,15 +1,14 @@
-
-
 def prefix(s):
-    v = [0]*len(s)
+    v = [0] * len(s)
     for i in range(1,len(s)):
-        k = v[i-1]
+        k = v[i - 1]
         while k > 0 and s[k] != s[i]:
-            k = v[k-1]
+            k = v[k - 1]
         if s[k] == s[i]:
             k = k + 1
         v[i] = k
-    return k
+    return v
 
-s=input()
-print(prefix(s))    
+s = input()
+v = prefix(s)
+print(int((v[-1] / v.count(0)) + 1)) 
