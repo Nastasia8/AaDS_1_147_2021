a19764cde615e2dbece67234a673b8103a97eb6b
pkubysheva <71191097+pkubysheva@users.noreply.github.com>
2021-04-05 20:27:24 +0300

    Add files via upload

---
+++ modul4(2).py
@@ -0,0 +1,17 @@
+from collections import deque 
+n=int(input())
+a=deque(list(map(int,input().split(maxsplit=n))))
+b=[]
+deadlock=[]
+res=list(range(1,n+1))
+while a:
+    if not deadlock or deadlock[-1]>a[0]:
+        deadlock.append(a.popleft())
+    if a and deadlock[-1]<a[0]:
+        b.append(deadlock.pop())
+while deadlock:
+    b.append(deadlock.pop())
+if b==res:
+    print("YES")
+else:
+    print("NO")
