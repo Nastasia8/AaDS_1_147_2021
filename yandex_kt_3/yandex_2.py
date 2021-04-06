17a8312f5425fa0257a2f68ce7641bd5e9485dce
Kooperatorko <71215527+Kooperatorko@users.noreply.github.com>
2021-04-06 20:56:59 +0300

    Create yandex_2.py

---
+++ yandex_kt_3/yandex_2.py
@@ -0,0 +1,14 @@
+# Ошибка TL
+
+def func(T, S):
+    for i in range(len(S)+1):
+        if S != T:
+            S = S[-1:] + S[:-1]
+        else:
+            return(i)
+    return('-1')
+
+S = str(input())
+T = str(input())
+
+print(func(T, S))
