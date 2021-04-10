a6cef4257350c6e76df930843f730b20f7eb38e0
Kooperatorko <71215527+Kooperatorko@users.noreply.github.com>
2021-04-10 20:27:00 +0300

    Update yandex_1.py

--- yandex_kt_3/yandex_1.py
+++ yandex_kt_3/yandex_1.py
@@ -1,8 +1,19 @@
-S = str(input())
-T = str(input())
-k = 0
+def func(S, T):
+    arr = [0] * len(S)
+    l = r = 0 
+    for i in range(1, len(S)):
+        if i <= r:
+            arr[i] = min(arr[i-l], r-i+1)
+        while arr[i]+i < len(S) and S[arr[i]] == S[arr[i]+i]:
+            arr[i] += 1
+        if arr[i]+i-1 > r:
+            l = i
+            r = arr[i] + i - 1
+    for i in range(len(arr)):
+        if arr[i] == len(T):
+            print (i-len(T)-1, end=' ')
 
-while k + len(T) <= len(S):
-    if S[k:k + len(T)] == T:
-        print(k, end = ' ')
-    k = k + 1
+S = str(input()) 
+T = str(input()) 
+S = T + '&' + S 
+func(S, T) 
