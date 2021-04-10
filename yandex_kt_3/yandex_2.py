4c26234026c1882f60307a7fbb36c292f814979e
Kooperatorko <71215527+Kooperatorko@users.noreply.github.com>
2021-04-10 20:27:42 +0300

    Update yandex_2.py

--- yandex_kt_3/yandex_2.py
+++ yandex_kt_3/yandex_2.py
@@ -1,14 +1,28 @@
-# Ошибка TL
+def hsht(S, p, x):
+    res = 0
+    for i in range(len(S)):
+        res = (res*x + ord(S[i])) % p
+    return res
 
-def func(T, S):
-    for i in range(len(S)+1):
-        if S != T:
-            S = S[-1:] + S[:-1]
+def func(S, T):
+    p = 10**9 + 7
+    x = 26
+    hs = hsht(S, p, x)
+    ht = hsht(T, p, x)
+    xt = 1
+    for i in range(len(S)-1):
+        xt = (xt*x) % p
+    for i in range(len(S)):
+        if hs == ht:
+            return i
         else:
-            return(i)
-    return('-1')
-
+            ht = (x * (ht - ord(T[i]) * xt) + ord(T[i])) % p
+    if hs != ht:
+        return('-1')
+        
 S = str(input())
 T = str(input())
-print(func(T, S))
-
+if not S == T:
+    print(func(S, T))
+else:
+    print('0')
