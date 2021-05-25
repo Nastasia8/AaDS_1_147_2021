301053066934aea618a9a342587780b3df4be5de
Nikrybkin <71150246+Nikrybkin@users.noreply.github.com>
2021-05-25 22:30:01 +0300

    Add files via upload

---
+++ Yandex-5.3.py
@@ -0,0 +1,39 @@
+from math import gcd
+
+
+def build(v, l, r, it, number):  # создаем функцию с переменными
+    if r-l == 1:
+        it[v] = number[l]
+        return
+    m = (r+l)//2  # формула поиска середины
+    build(2*v+1, l, m, it, number)
+    build(2*v+2, m, r, it, number)
+    it[v] = gcd(it[2*v+1], it[2*v+2])
+
+
+def cal(v, l, r, it, ql, qr):  # функия, для поиска НОД слева и справа
+    if ql <= l and qr >= r:
+        return it[v]
+    if ql >= r or qr <= l:
+        return 0
+    m = (r+l)//2  # формула поиска середины
+    tl = cal(2*v+1, l, m, it, ql, qr)
+    tr = cal(2*v+2, m, r, it, ql, qr)
+    return gcd(tl, tr)
+
+
+def main():
+    n = int(input())  # ввод данных по условиям
+    number = list(map(int, input().split()))  # создание списка
+    it = [0]*4*n
+    build(0, 0, n, it, number)
+    q = int(input())
+    index = []
+    while q != 0:  # выполняем условие, пока оно не равно 0
+        l, r = map(int, input().split())
+        index.append(cal(0, 0, n, it, l-1, r))
+        q -= 1
+    print(*index)
+
+
+main()
