04b32cbe1f264319eaae795da7fecaec5fcc2ad9
swbonjour <71038541+swbonjour@users.noreply.github.com>
2021-05-27 16:22:40 +0300

    Update yandex5.py

--- yandex5.py
+++ yandex5.py
@@ -113,3 +113,41 @@ if balance_tree(tree):
     print("YES")
 else:
     print("NO")
+
+#3
+
+from math import gcd
+
+def build(v, l, r, it, number): #строим дерево
+    if r-l == 1:
+        it[v] = number[l]
+        return
+    m = (r+l)//2
+    build(2*v+1, l, m, it, number)
+    build(2*v+2, m, r, it, number)
+    it[v] = gcd(it[2*v+1], it[2*v+2])
+
+def calc(v, l, r, it, ql, qr): #просчет НОД слева и справа
+    if ql <= l and qr >= r:
+        return it[v]
+    if ql >= r or qr <= l:
+        return 0
+    m = (r+l)//2
+    tl = calc(2*v+1, l, m, it, ql, qr)
+    tr = calc(2*v+2, m, r, it, ql, qr)
+    return gcd(tl, tr)
+
+def main():
+    n = int(input())
+    number = list(map(int, input().split()))
+    it = [0]*4*n
+    build(0, 0, n, it, number)
+    q = int(input())
+    index = []
+    while q != 0: #выполняем условие, пока оно не равно 0
+        l, r = map(int, input().split())
+        index.append(calc(0, 0, n, it, l-1, r))
+        q -= 1
+    print(*index)
+
+main()
