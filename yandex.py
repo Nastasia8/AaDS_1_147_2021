6ed11d32ef67a75f6f4d8e995d40b67ae5518cdd
swbonjour <71038541+swbonjour@users.noreply.github.com>
2021-05-28 20:14:09 +0300

    Update yandex.py

--- yandex.py
+++ yandex.py
@@ -36,45 +36,63 @@ for i in range(len(a)):
     print(*a[i])
 
 #sort 3
+  
+def main():
+    n = int(input())
+    a = list(map(int,input().split()))[:n]
+    merge(a,0,len(a))
+    print(*a,sep=" ")
+def merge(a,start,end):
+    if end - start > 1:
+        middle = (start + end)//2
+        merge(a,start,middle)
+        merge(a,middle,end)
+        left = a[start:middle]
+        right = a[middle:end]
+        internal_sort(a,left,right,start)
+        print(start+1,end,a[start],a[end-1])
+def internal_sort(arr,left,right,start):
+    j = i = 0
+    k = start
+    while i<len(right) and j<len(left):
+        if left[j] > right[i]:
+            arr[k] = right[i]
+            i+=1
+        else:
+            arr[k] = left[j]
+            j+=1
+        k+=1
+    while j<len(left):
+        arr[k] = left[j]
+        j+=1
+        k+=1
+    while i<len(right):
+        arr[k] = right[i]
+        i+=1
+        k+=1
+main()
+
+#sort 4
+
+def count_inversions(a):
+  res = 0
+  counts = [0]*(len(a)+1)
+  rank = { v : i+1 for i, v in enumerate(sorted(a)) }
+  for x in reversed(a):
+    i = rank[x] - 1
+    while i:
+      res += counts[i]
+      i -= i & -i
+    i = rank[x]
+    while i <= len(a):
+      counts[i] += 1
+      i += i & -i
+  return res
+
 N = int(input())
 numbers = [int(i) for i in input().split()][:N]
 
-def merge_two_lists(left, right):
-    a = []
-    i = j = 0
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            a.append(left[i])
-            i += 1
-        else:
-            a.append(right[j])
-            j += 1
-            
-    if i < len(left):
-        a += left[i:]
-    if j < len(right):
-        a += right[j:]
-
-    index_left = i + 1
-    index_left_value = a[i]
-
-    index_right = j + 1
-    index_right_value = a[j]
-
-    print(index_left, index_right, index_left_value, index_right_value)
-    
-    return a
-
-def merge_sort(numbers):
-    if len(numbers) == 1:
-        return numbers
-
-    middle = len(numbers)//2
-    left = merge_sort(numbers[:middle])
-    right = merge_sort(numbers[middle:])
-    return merge_two_lists(left, right)
-
-print(*merge_sort(numbers))
+print(count_inversions(numbers))
 
 #sort 5
 
