4d7f877b89a3c42155889d6af55977042a89235a
swbonjour <71038541+swbonjour@users.noreply.github.com>
2021-05-28 20:12:21 +0300

    Update yandex.py

--- yandex.py
+++ yandex.py
@@ -76,6 +76,44 @@ def merge_sort(numbers):
 
 print(*merge_sort(numbers))
 
+#sort 4
+
+def main():
+     n = int(input())
+     array = list(map(int,input().split()))
+     print (count_inversion(array))
+
+
+def count_inversion(lst):
+    return merge_count_inversion(lst)[1]
+
+def merge_count_inversion(lst):
+    if len(lst) <= 1:
+        return lst, 0
+    middle = int( len(lst) / 2 )
+    left, a = merge_count_inversion(lst[:middle])
+    right, b = merge_count_inversion(lst[middle:])
+    result, c = merge_count_split_inversion(left, right)
+    return result, (a + b + c)
+
+def merge_count_split_inversion(left, right):
+    result = []
+    count = 0
+    i, j = 0, 0
+    left_len = len(left)
+    while i < left_len and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            count += left_len - i
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result, count        
+main()
+
 #sort 5
 
 N = int(input())
