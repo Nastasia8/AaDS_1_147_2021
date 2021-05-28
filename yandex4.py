24b0d234a2bba0abadc700e8c2578c2ba20a9845
swbonjour <71038541+swbonjour@users.noreply.github.com>
2021-05-28 20:17:12 +0300

    Update yandex4.py

--- yandex4.py
+++ yandex4.py
@@ -40,23 +40,18 @@ else:
     
 #3
 
-N = int(input())
-numbers = [int(i) for i in input().split()][:N]
 
-def near_number(numbers):
-    
-    res = []
-    
-    for i in range(len(numbers)-1):
-        for j in range(i, len(numbers)):
-            if numbers[i] > numbers[j]:
-                res.append(j)
-                break
-            if j == len(numbers)-1:
-                res.append(-1)
-                
-    res.append(-1)
-        
-    return res
-
-print(*near_number(numbers))
+n = int(input())
+numbers = list(map(int, input().split(maxsplit = n)))
+d = {i:numbers[i] for i in range(n)}
+stack = []
+index = [0]*n
+for i in range(n-1, -1, -1):
+    while stack and stack[-1][1] >= d[i]:
+        stack.pop()
+    if not stack:
+        index[i] = -1
+    else:
+        index[i] = stack[-1][0]
+    stack.append([i, d[i]])
+print(*index)
